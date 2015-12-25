0f49157b9f83c0b6bfb670c81d7eaed73603196a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-12-25 09:53:16 -0500

    Separate read/write ports, now reports "unconstrained path" again

--- read_write.v
+++ read_write.v
@@ -5,7 +5,8 @@ module counter(
     i__pkt_1,
     i__sel,
 
-    o__write__pff,
+    o__write,
+    o__read,
 );
 
 // Parameters
@@ -17,11 +18,12 @@ input  logic [COUNT_WIDTH-1:0]          i__pkt_1;
 input  logic                            i__sel;
 
 // Sequential elements
-logic [COUNT_WIDTH-1:0]                 r__write__pff;
+logic [COUNT_WIDTH-1:0]                 r__register__pff;
 input  logic clk;
 
 // Output signals
-output logic [COUNT_WIDTH-1:0]          o__write__pff;
+output logic [COUNT_WIDTH-1:0]          o__write;
+output logic [COUNT_WIDTH-1:0]          o__read;
 
 // 2-way mux
 function logic [COUNT_WIDTH-1:0] mux (input logic [COUNT_WIDTH-1:0] x, input logic [COUNT_WIDTH-1:0] y, input logic sel);
@@ -36,13 +38,15 @@ endfunction
 //------------------------------------------------------------------------------
 always_comb
 begin
-    o__write__pff = mux(i__constant, i__pkt_1, i__sel);
+  o__read  = r__register__pff;
+  o__write = mux(i__constant, i__pkt_1, i__sel);
 end
+
 //------------------------------------------------------------------------------
 //
 always_ff @ (posedge clk)
 begin
-  r__write__pff <= o__write__pff;
+  r__register__pff <= o__write;
 end
 
 endmodule
