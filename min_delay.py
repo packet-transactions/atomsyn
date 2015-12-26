6d8ba7b30adb5ad8cc6648a644bf469ec958f22c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-12-25 21:53:09 -0500

    Binary search logic

--- min_delay.py
+++ min_delay.py
@@ -7,7 +7,6 @@ import subprocess
 # cmd line args
 synth_lib = sys.argv[1]
 atom_file = sys.argv[2]
-clock_ns  = sys.argv[3]
 
 # Program wrapper
 # Takes a command line of program arguements,
@@ -25,13 +24,22 @@ def program_wrapper(program, t_stdout = subprocess.PIPE, t_stderr = subprocess.P
     print " ".join(program), " completed with return code of zero"
     return (out, err)
 
-# Run Synopsys DC
-arg_str =  "DESIGN_RTL=" + atom_file + " " + \
+# Run Synopsys DC, using bisection search to find the min. clock
+left_end    = 1
+right_end   = 1000
+cur_clock   = (left_end + right_end) / 2.0
+
+while (left_end < right_end):
+  cur_clock = int((left_end + right_end) / 2.0)
+  arg_str =  "DESIGN_RTL=" + atom_file + " " + \
            "DESIGN_TOPLEVEL=" + atom_file.split(".")[0] + " " + \
            "SYNTH_LIB=" + synth_lib + " " + \
-           "CLOCK_PERIOD=" + str(clock_ns) + " " + \
+           "CLOCK_PERIOD=" + str(cur_clock) + " " + \
            "dc_shell -f synth.tcl"
-out, err = program_wrapper(arg_str);
-
-print out
-print err
+  out, err = program_wrapper(arg_str);
+  if "slack (MET)" in out:
+    print "Met timing at ", cur_clock
+    right_end = cur_clock - 1;
+  else :
+    print "Failed timing at ", cur_clock
+    left_end  = cur_clock + 1;
