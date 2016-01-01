77c4c2971815f2786d20a17bc920bf2a7d9397f7
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-01-01 16:55:46 -0500

    Get rid of set_output_delay;
    I didn't have a great reason for this in the first place.

--- synth.tcl
+++ synth.tcl
@@ -82,7 +82,6 @@ if { [catch {
   run_dc_cmd "create_clock -period $env(CLOCK_PERIOD) -name master_clk clk"
   run_dc_cmd "set_clock_uncertainty 40 -hold {[get_object_name [all_clocks]]}"
   run_dc_cmd "set_clock_uncertainty 40 -setup {[get_object_name [all_clocks]]}"
-  run_dc_cmd "set_output_delay -clock master_clk 0 [all_outputs]"
   run_dc_cmd "set_max_transition  40 {[get_object_name [all_inputs]]}"
   run_dc_cmd "set_max_transition  40 {[get_object_name [all_outputs]]}"
   run_dc_cmd "set_load -pin_load  10 {[get_object_name [all_outputs]]}"
