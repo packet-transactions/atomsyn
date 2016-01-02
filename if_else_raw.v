3760b1cfb17ca5407ca57793ac45aac4eb325f06
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-01-01 19:37:47 -0500

    Rewrite if_else_raw to use register inputs

--- if_else_raw.v
+++ if_else_raw.v
@@ -6,43 +6,52 @@ typedef logic[1:0] int2_t;
 module if_else_raw(
     clk,
 
-    pkt_1,
-    pkt_2,
-    cons_1,
-    cons_2,
-    cons_3,
- 
-    sel_1,
-    sel_2,
-    sel_3,
-    sel_4,
-    sel_5,
-    sel_6,
-    rel_opcode,
+    i__pkt_1,
+    i__pkt_2,
+    i__cons_1,
+    i__cons_2,
+    i__cons_3,
+    i__sel_1,
+    i__sel_2,
+    i__sel_3,
+    i__sel_4,
+    i__sel_5,
+    i__sel_6,
+    i__rel_opcode,
 
     o__write,
     o__read
 );
 
 // Input signals
-input  int32_t          pkt_1;
-input  int32_t          pkt_2;
-input  int32_t          cons_1;
-input  int32_t          cons_2;
-input  int32_t          cons_3;
-
-input  bool             sel_1;
-input  int2_t           sel_2;
-input  bool             sel_3;
-input  int2_t           sel_4;
-input  bool             sel_5;
-input  int2_t           sel_6;
-
-input  int2_t           rel_opcode;
+input  int32_t          i__pkt_1;
+input  int32_t          i__pkt_2;
+input  int32_t          i__cons_1;
+input  int32_t          i__cons_2;
+input  int32_t          i__cons_3;
+input  bool             i__sel_1;
+input  int2_t           i__sel_2;
+input  bool             i__sel_3;
+input  int2_t           i__sel_4;
+input  bool             i__sel_5;
+input  int2_t           i__sel_6;
+input  int2_t           i__rel_opcode;
 
 // Sequential elements
 int32_t state_1;
 input  logic clk;
+int32_t          pkt_1;
+int32_t          pkt_2;
+int32_t          cons_1;
+int32_t          cons_2;
+int32_t          cons_3;
+bool             sel_1;
+int2_t           sel_2;
+bool             sel_3;
+int2_t           sel_4;
+bool             sel_5;
+int2_t           sel_6;
+int2_t           rel_opcode;
 
 // Output signals
 output int32_t o__write;
@@ -97,6 +106,18 @@ end
 always_ff @ (posedge clk)
 begin
   state_1 <= o__write;
+  pkt_1 <= i__pkt_1;
+  pkt_2 <= i__pkt_2;
+  cons_1 <= i__cons_1;
+  cons_2 <= i__cons_2;
+  cons_3 <= i__cons_3;
+  sel_1  <= i__sel_1;
+  sel_2  <= i__sel_2;
+  sel_3  <= i__sel_3;
+  sel_4  <= i__sel_4;
+  sel_5  <= i__sel_5;
+  sel_6  <= i__sel_6;
+  rel_opcode <= i__rel_opcode;
 end
 
 endmodule
